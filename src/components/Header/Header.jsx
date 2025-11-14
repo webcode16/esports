757dcf10bf45644a8897dea2a3c328f65bddf6d5
ankit <ankit15code@gmail.com>
2025-11-14 16:29:40 +0530

    Initial commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
-import React from "react";
 import { Link } from "react-router-dom";
-// import logo from "../assets/logo.png";
+import logo from "../../assets/logo.png";
+import "./Header.scss";
 const Header = () => {
   return (
    <header className="header">
+    <div className="page-width d-flex align-center justify-space-between header-inside">
       <div className="logo">
         <Link to="/">
-          {/* <img src={logo} alt="MyApp Logo" className="logo-img" /> */}
+          <img src={logo} alt="MyApp Logo" className="logo-img" />
         </Link>
       </div>
       <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
       </nav>
+    </div> 
     </header>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.scss'
 import App from './App.jsx'
-
+import "./style/global.scss";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <BrowserRouter>
